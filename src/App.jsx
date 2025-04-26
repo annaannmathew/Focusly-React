5db3a42ab17405f45f70306f0a3016dd0c8bf724
Anna Ann Mathew <85623708+annaannmathew@users.noreply.github.com>
2025-04-26 10:29:46 +0530

    App page Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,3 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-// import Login from "./component/Login";
-// import Register from "./component/Register";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         {/* Default route redirects to /login */}
-//         <Route path="/" element={<Navigate to="/login" />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-//         {/* You can add a dashboard or 404 route later */}
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import AuthForm from "./component/AuthForm";
@@ -28,15 +6,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Redirect root to /login */}
         <Route path="/" element={<Navigate to="/login" />} />
-        
-        {/* Login and Register both handled by AuthForm */}
         <Route path="/login" element={<AuthForm />} />
         <Route path="/register" element={<AuthForm />} />
-
-        {/* You can add more routes like Dashboard later */}
-        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       </Routes>
     </Router>
   );
